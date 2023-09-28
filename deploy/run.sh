cedebc07a0a265966a48a3c2d7742069b06f5662
christopherpettinga <chrispettinga@gmail.com>
2023-09-28 14:44:29 +0100

    added requirements file

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,12 +1,10 @@
 #!/bin/bash -e
 
-cd ${POP_DIRECTORY}/deploy
-
 python create_process_json.py
 python create_nginx_conf.py
 
 
-cd ${POP_DIRECTORY}
+cd ..
 pm2 delete all
 pm2 start process.json
 
