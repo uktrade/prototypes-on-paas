382a4eb5ba42a6f0c9bf8114eec120abf1bfc535
christopherpettinga <chrispettinga@gmail.com>
2023-10-04 10:24:43 +0100

    removed todo

--- prototypes/proto-1/app/assets/javascripts/jquery.js
+++ prototypes/proto-1/app/assets/javascripts/jquery.js
@@ -4119,7 +4119,6 @@ var dataUser = new Data();
 //	2. Improve the module's maintainability by reducing the storage
 //		paths to a single mechanism.
 //	3. Use the same single mechanism to support "private" and "user" data.
-//	4. _Never_ expose "private" data to user code (TODO: Drop _data, _removeData)
 //	5. Avoid exposing implementation details on user objects (eg. expando properties)
 //	6. Provide a clear path for implementation upgrade to WeakMap in 2014
 
@@ -4187,7 +4186,6 @@ jQuery.extend( {
 		dataUser.remove( elem, name );
 	},
 
-	// TODO: Now that all calls to _data and _removeData have been replaced
 	// with direct calls to dataPriv methods, these can be deprecated.
 	_data: function( elem, name, data ) {
 		return dataPriv.access( elem, name, data );
