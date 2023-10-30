db8c7376a241536ff6eb5a310d08da247642438f
christopherpettinga <chrispettinga@gmail.com>
2023-10-30 17:51:19 +0000

    test

--- prototypes_router/core/settings.py
+++ prototypes_router/core/settings.py
@@ -24,9 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret! SECRET I SAY
 SECRET_KEY = env.str("SECRET_KEY")
-
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", False)
